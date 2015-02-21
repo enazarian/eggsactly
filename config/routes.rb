a4566a1bb9c40780e54c3707f24630bf8be37022
enazarian <esnazarian@gmail.com>
2015-02-20 19:47:50 -0600

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -151,14 +151,17 @@ Rails.application.routes.draw do
   # # DELETE
   # delete '/users/:id',   :controller => 'users', :action => 'destroy'
 
+
+
   devise_for :users
 
   devise_scope :user do
     authenticated :user do
       root :to => 'products#index', as: :authenticated_root
     end
+
     unauthenticated :user do
-      root :to => 'devise/registrations#new', as: :unauthenticated_root
+      root :to => 'static_pages#home', as: :home
     end
   end
 
