515533e64ed896434891d713c969809c00d6e255
enazarian <esnazarian@gmail.com>
2015-02-20 19:56:22 -0600

    fixed route errors

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,5 +1,5 @@
 class Source < ActiveRecord::Base
-  has_many :stores => :through :products_at_store_from_source
+  has_many :stores, through: :products_at_store_from_source
   has_many :products_from_source
   has_many :products_at_store_from_source
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,5 +1,5 @@
 class Store < ActiveRecord::Base
-  has_many :sources => :through :products_at_store_from_source
+  has_many :sources, through: :products_at_store_from_source
   has_many :products_at_store
   has_many :products_at_store_from_source
 end
